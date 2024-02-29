21a659ba572d8e21613df9f47c4a920bf1b6e33a
Guilherme Henrique <guiihdourado@gmail.com>
2024-02-29 18:08:29 -0300

    feat: add styles

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,10 +17,17 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px ;
+  border: 0;
+  margin: 8px;
 
-  ${props => {
+
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
+
+  /*${props => {
     return css`
   background-color: ${buttonVariants[props.variant]}
   `
-  }}
+  }}*/
 `
