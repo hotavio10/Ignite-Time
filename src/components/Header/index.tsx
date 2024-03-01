ccfc4a4ba8b403cf205fcb32a69de0719903e034
Otavio Miranda <hotavio10@gmail.com>
2024-03-01 17:08:06 -0300

    feat: add Header & Layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContaienr } from './styles'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContaienr>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Historico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContaienr>
+  )
+}
