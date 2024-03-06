822b4f35c3aa3807f4badcb0f225b1e95266af0d
Otavio Miranda <hotavio10@gmail.com>
2024-03-06 19:33:35 -0300

    feat: add validating forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -11,12 +14,22 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+task: zod.string().min(1, 'Informe a tarefa'),
+MinutesAmoun: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
 export function Home() {
-const { register, handleSubmit, watch  } = useForm()
+const { register, handleSubmit, watch, formState  } = useForm({
+  resolver: zodResolver(newCycleFormValidationSchema),
+})
 
 function handleCreateNewCycle(data: any) {
   console.log(data)
 }
+
+console.log(formState.errors)
+
 const task = watch('task')
 const isSubmitDisabled = !task
 
