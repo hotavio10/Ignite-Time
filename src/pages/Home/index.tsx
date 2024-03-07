ec9b5412254f9c8721ba2f7cb7ac1059579f7ef1
Otavio Miranda <hotavio10@gmail.com>
2024-03-07 20:37:44 -0300

    feat: pre update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -15,62 +15,70 @@ import {
 } from './styles'
 
 const newCycleFormValidationSchema = zod.object({
-task: zod.string().min(1, 'Informe a tarefa'),
-minutesAmount: zod
-.number()
-.min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
-.max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
-export function Home() {
-const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-  resolver: zodResolver(newCycleFormValidationSchema),
-  defaultValues: {
-    task: '',
-    minutesAmount: 0,
-  },
-})
-
-function handleCreateNewCycle(data: NewCycleFormData) {
-  console.log(data)
-  reset()
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
 }
 
-const task = watch('task')
-const isSubmitDisabled = !task
+export function Home() {
+  const [] = useState()
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">vou trabalhar em </label>
-          <TaskInput 
-          id="task" 
-          list="task-suggestions" 
-          placeholder="Dê um nome para o seu projeto"
-          {...register('task')}
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
 
           <datalist id="task-suggestions">
-            <option value="Projeto 1"/>
-            <option value="Projeto 2"/>
-            <option value="Projeto 3"/>
-            <option value="Banana"/>
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmounInput 
-          type="number"
+          <MinutesAmounInput
+            type="number"
             id="minutesAmount"
-            placeholder="00" 
+            placeholder="00"
             step={5}
             min={5}
             max={60}
-            {...register('minutesAmount', { valueAsNumber: true})}
-            />
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
 
           <span>minutos.</span>
         </FormContainer>
