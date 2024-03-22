094396d7cfbff4905fcde74c2ec711c425f270b2
hotavio10 <hotavio10@gmail.com>
2024-03-22 17:14:29 -0300

    feat: add interrupt cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -76,7 +76,7 @@ export const CountdownContainer = styled.div`
 `
 export const Separator = styled.div`
   padding: 2rem;
-  color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-300']};
 
   width: 4rem;
   overflow: hidden;
@@ -84,7 +84,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
 width: 100%;
 border: 0;
 padding: 1rem;
@@ -93,23 +93,31 @@ border-radius: 8px;
 display: flex;
 align-items: center;
 justify-content: center;
+color: ${(props) => props.theme['gray-100']};
 
 gap: 0,5rem;
 font-weight: bold;
 
 cursor: pointer;
 
-background: ${(props) => props.theme['green-500']};
-color: ${(props) => props.theme['gray-100']};
-
-
 &:disabled {
   opacity: 0.7;
   cursor: not-allowed;
 }
-
-&:hover {
-  background: ${(props) => props.theme['green-500']};
-}
-
 `
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+background: ${(props) => props.theme['green-500']};
+
+&:not(:disabled):hover {
+  background: ${(props) => props.theme['green-700']};
+}
+`;
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+background: ${(props) => props.theme['red-500']};
+
+&:not(:disabled):hover {
+  background: ${(props) => props.theme['red-700']};
+}
+`;
